d31171dca375370911a42479c4bb3bad83a2a685
liner0211 <66686271+liner0211@users.noreply.github.com>
2024-07-30 15:47:26 -0500

    Update installtheos.sh

--- installtheos.sh
+++ installtheos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export THEOS=/opt/theos >> ~/.profile
+echo export THEOS=/opt/theos >> ~/.profile
 echo “if [[ "$(umask)" = "0000" ]]; then” >> ~/.profile
 echo “  umask 0022” >> ~/.profile
 echo “fi” >> ~/.profile
