b7d9ef0d7175d404169284f324624236d57bd94f
liner0211 <66686271+liner0211@users.noreply.github.com>
2024-07-30 15:31:58 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export THEOS=/opt/theos >> ~/.profile
+export THEOS=/home/liner0211/theos >> ~/.profile
 echo “if [[ "$(umask)" = "0000" ]]; then” >> ~/.profile
 echo “  umask 0022” >> ~/.profile
 echo “fi” >> ~/.profile
