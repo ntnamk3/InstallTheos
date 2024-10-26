647f12789f332585c7261361cbfb6b949c50385c
Nem'G <74763776+ntnamk3@users.noreply.github.com>
2024-10-25 18:16:45 -0700

    Update installtheos.sh

--- installtheos.sh
+++ installtheos.sh
@@ -25,62 +25,15 @@ fi
 # Source the profile to apply changes
 source ~/.profile
 
-#sudo apt-get update
-#sudo apt-get install -y software-properties-common gnupg1 gnupg2 gnupg3 gnupg unzip
+# Install required packages
+yum install -y epel-release
+yum install -y clang git perl unzip fakeroot build-essential wget
 
+# Set THEOS path and clean previous installations
 sudo rm -rf $THEOS
+LLVM_VERSION=${1:-16}
 
-LLVM_VERSION=${1:-16}  # Updated to the latest LLVM version
-
-DISTRO=$(lsb_release -is)
-VERSION=$(lsb_release -sr)
-DIST_VERSION="${DISTRO}_${VERSION}"
-
-declare -A LLVM_VERSION_PATTERNS
-LLVM_VERSION_PATTERNS[9]="-9"
-LLVM_VERSION_PATTERNS[10]="-10"
-LLVM_VERSION_PATTERNS[11]="-11"
-LLVM_VERSION_PATTERNS[12]="-12"
-LLVM_VERSION_PATTERNS[13]="-13"
-LLVM_VERSION_PATTERNS[14]="-14"
-LLVM_VERSION_PATTERNS[15]="-15"
-LLVM_VERSION_PATTERNS[16]="-16"
-
-if [ ! ${LLVM_VERSION_PATTERNS[$LLVM_VERSION]+_} ]; then
-    echo "This script does not support LLVM version $LLVM_VERSION"
-    exit 3
-fi
-
-LLVM_VERSION_STRING=${LLVM_VERSION_PATTERNS[$LLVM_VERSION]}
-
-# find the right repository name for the distro and version
-case "$DIST_VERSION" in
-    Debian_9* )       REPO_NAME="deb http://apt.llvm.org/stretch/  llvm-toolchain-stretch$LLVM_VERSION_STRING main" ;;
-    Debian_10* )      REPO_NAME="deb http://apt.llvm.org/buster/   llvm-toolchain-buster$LLVM_VERSION_STRING  main" ;;
-    Debian_unstable ) REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
-    Debian_testing )  REPO_NAME="deb http://apt.llvm.org/unstable/ llvm-toolchain$LLVM_VERSION_STRING         main" ;;
-    Ubuntu_16.04 )    REPO_NAME="deb http://apt.llvm.org/xenial/   llvm-toolchain-xenial$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_18.04 )    REPO_NAME="deb http://apt.llvm.org/bionic/   llvm-toolchain-bionic$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_18.10 )    REPO_NAME="deb http://apt.llvm.org/cosmic/   llvm-toolchain-cosmic$LLVM_VERSION_STRING  main" ;;
-    Ubuntu_19.04 )    REPO_NAME="deb http://apt.llvm.org/disco/    llvm-toolchain-disco$LLVM_VERSION_STRING   main" ;;
-    Ubuntu_19.10 )    REPO_NAME="deb http://apt.llvm.org/eoan/     llvm-toolchain-eoan$LLVM_VERSION_STRING    main" ;;
-    Ubuntu_20.04 )    REPO_NAME="deb http://apt.llvm.org/focal/    llvm-toolchain-focal$LLVM_VERSION_STRING   main" ;;
-    Ubuntu_22.04 )    REPO_NAME="deb http://apt.llvm.org/jammy/    llvm-toolchain-jammy$LLVM_VERSION_STRING   main" ;;
-    * )
-        echo "Distribution '$DISTRO' in version '$VERSION' is not supported by this script (${DIST_VERSION})."
-        exit 2
-esac
-
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo add-apt-repository "${REPO_NAME}"
-sudo apt-get update
-sudo apt-get install -y clang-$LLVM_VERSION lldb-$LLVM_VERSION lld-$LLVM_VERSION clangd-$LLVM_VERSION
-sudo apt-get install -y fakeroot git perl build-essential
-
-sudo git clone --recursive https://github.com/theos/theos.git $THEOS
-sudo rm -rf $THEOS/toolchain*
-
-# Replace with the latest toolchain link
+# Download and install the latest LLVM toolchain for CentOS
 curl -LO https://github.com/sbingner/llvm-project/releases/download/v16.0.0-1/linux-ios-arm64e-clang-toolchain.tar.lzma
 TMP=$(mktemp -d)
 echo $TMP
@@ -89,6 +42,11 @@ sudo mkdir -p $THEOS/toolchain/linux/iphone
 sudo mv $TMP/ios-arm64e-clang-toolchain/* $THEOS/toolchain/linux/iphone/
 rm -rf $TMP linux-ios-arm64e-clang-toolchain.tar.lzma
 
+# Clone Theos repository
+sudo git clone --recursive https://github.com/theos/theos.git $THEOS
+sudo rm -rf $THEOS/toolchain*
+
+# Download iOS SDKs
 cd $HOME
 wget https://github.com/xybp888/iOS-SDKs/archive/master.zip
 unzip master.zip
@@ -96,7 +54,7 @@ sudo mkdir -p $THEOS/sdks
 sudo mv $HOME/iOS-SDKs-master/*.sdk $THEOS/sdks
 rm -rf $HOME/iOS-SDKs-master $HOME/master.zip
 
-# Replace with the latest Swift toolchain link
+# Download and install Swift toolchain
 curl https://kabiroberai.com/toolchain/download.php?toolchain=swift-ubuntu-latest -Lo swift-toolchain.tar.gz
 sudo tar xzf swift-toolchain.tar.gz -C $THEOS/toolchain
 rm swift-toolchain.tar.gz
